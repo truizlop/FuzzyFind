c019279d7d0b2780541ff39ee21ca6a4a9052ce3
Tomás Ruiz-López <truizlop@gmail.com>
2021-03-27 11:35:36 +0100

    Add tests

--- Tests/FuzzyFindTests/FuzzyFindTests.swift
+++ Tests/FuzzyFindTests/FuzzyFindTests.swift
@@ -2,14 +2,43 @@ import XCTest
 @testable import FuzzyFind
 
 final class FuzzyFindTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FuzzyFind().text, "Hello, World!")
+    func testMatch() {
+        let actual = bestMatch(query: "ff", input: "FuzzyFind")
+        let expected = Alignment(
+            score: 55,
+            result: FuzzyResult(segments: [
+                .match(["F"]),
+                .gap(["u", "z", "z", "y"]),
+                .match(["F"]),
+                .gap(["i"]),
+                .gap(["n"]),
+                .gap(["d"])
+            ])
+        )
+        XCTAssertEqual(actual, expected)
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testContiguousCharactersHaveHigherScore() {
+        let a1 = bestMatch(query: "pp", input: "pickled pepper")!
+        let a2 = bestMatch(query: "pp", input: "Pied Piper")!
+        XCTAssertTrue(a1.score > a2.score)
+    }
+
+    func testCharactersAtBeginningOfWordsHaveHigherScore() {
+        let a1 = bestMatch(query: "pp", input: "Pied Piper")!
+        let a2 = bestMatch(query: "pp", input: "porcupine")!
+        XCTAssertTrue(a1.score > a2.score)
+    }
+
+    func testCamelCaseHumpsHaveHigherScore() {
+        let a1 = bestMatch(query: "bm", input: "BatMan")!
+        let a2 = bestMatch(query: "bm", input: "Batman")!
+        XCTAssertTrue(a1.score > a2.score)
+    }
+
+    func testFirstLettersOfWordsHaveHigherScore() {
+        let a1 = bestMatch(query: "bm", input: "Bat man")!
+        let a2 = bestMatch(query: "bm", input: "Batman")!
+        XCTAssertTrue(a1.score > a2.score)
+    }
 }
